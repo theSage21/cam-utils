4ed7e1bdee4cd866ea833fd1df3e519e6dfc4f63
Arjoonn Sharma <arjoonn.94@gmail.com>
2016-07-31 22:49:31 +0530

    Added support for JSON config files

--- run.py
+++ run.py
@@ -1,30 +1,32 @@
-import os
+# -----------------------------------------------
+# import the things we are going to need
+import json
 import cv2
-import base64
 from threading import Thread
 
-# what scripts to use
+# -----------------------------------------------
+# import scripts to be used
 from screen_brightness import run as screen_run
 from face_distance import run as face_dist_run
+
 # function , every_n_frames
 runnables = [(screen_run, 20),
              (face_dist_run, 15)]
-
-
-print('python run_script.py')
-
-#config options
-display = True  # display webcam output?
-#IP = 'http://192.168.11.10:8080/videofeed'  # IP camera option is available
-IP = None
+# -----------------------------------------------
+# Load configuration files
+print('python run_script.py| config.json is for configuration')
+config = json.loads(open('config.json', 'r').read())
 
 # -----------------------------------------------
+# run program
 
-if IP is not None:
-    video_capture = cv2.VideoCapture(IP)
-else:
+# Choose input device
+if config['cam'] == 'ip':
+    video_capture = cv2.VideoCapture(config['ip'])
+elif config['cam'] == 'inbuilt':
     video_capture = cv2.VideoCapture(0)
 
+# get frames and run scripts on them
 frame_count = 0
 while True:
     ret, frame = video_capture.read()
@@ -32,7 +34,7 @@ while True:
     for run, n_frames in runnables:
         if frame_count % n_frames == 0:
             Thread(target=run, args=(frame, )).run()
-    if display:
+    if config['show_video']:
         cv2.imshow('Video', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
